010847bfc5425b7440cc62c74374c151c322b240
Fernanda Lecaros Saavedra <fernandalecaros@MBARI2615.shore.mbari.org>
2024-09-17 17:02:49 -0700

    fix: uploading the weights into the model

--- classification.py
+++ classification.py
@@ -335,7 +335,7 @@ if (not os.path.exists('{}.pth'.format(args.name)) or remake_model):
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
 else:
     print("Previous model found, loading {}".format(args.name))
-    model = models.resnet18( weights=args.name)
+    model = models.resnet18( weights="{}.pth".format(args.name))
     #Modify the last layer of the model
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     model.load_state_dict(torch.load('{}.pth'.format(args.name)))
